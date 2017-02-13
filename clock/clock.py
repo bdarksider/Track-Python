ecfd77f6b9a620bd82bfce6373da205da24e590d
Bejoy Mathew <bejoygm@gmail.com>
2017-02-13 23:01:30 +0530

    basic clock added

---
+++ clock/clock.py
@@ -0,0 +1,6 @@
+class Clock:
+    def __init__(self, hour, min):
+        self.time = "08:00"
+    def __repr__(self):
+        return self.time 
+
---
+++ hello-world/hello_world.py
@@ -0,0 +1,8 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name=''):
+    return "Hello, {}!".format(name or "World")
+
