f70294769720b4fec6d14dfb8ee1412d2fb249f3
Bejoy Mathew <bejoygm@gmail.com>
2017-02-16 20:54:54 +0530

    clock question complete

--- clock/clock.py
+++ clock/clock.py
@@ -1,6 +1,17 @@
 class Clock:
     def __init__(self, hour, min):
-        self.time = "08:00"
+        self.min = (hour * 60 + min) % 60
+        self.hour = ((hour * 60 + min) // 60) % 24 
+
     def __repr__(self):
-        return self.time 
+        return "%02d:%02d" %(self.hour, self.min)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def add(self, min):
+        self.hour = ((self.hour * 60 + min + self.min) // 60) % 24
+        self.min = (self.min + min) % 60
+        return "%02d:%02d" %(self.hour, self.min)
+
 
